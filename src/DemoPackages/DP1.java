c3629001b074da38f2b2489ad57e5468b929ab32
mansi150896 <80631476+mansi150896@users.noreply.github.com>
2026-02-21 00:10:16 -0500

    Create DP1.java

---
+++ src/DemoPackages/DP1.java
@@ -0,0 +1,22 @@
+package DemoPackages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+		driver.findElement(By.xpath("//*[@id=\"APjFqb\"]")).sendKeys("ArtOfTesting");
+		driver.findElement(By.xpath("//*[@id=\"Alh6id\"]/div[1]/div/ul/li[1]/div")).click();
+
+	}
+
+}
